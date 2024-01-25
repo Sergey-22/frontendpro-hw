8d02b54155961bd5e83435bc020e5a2fcfa94fb8
Sergey <zolotarevs070@gmail.com>
2024-01-25 09:52:44 +0200

    hw-5

---
+++ main.js
@@ -0,0 +1,31 @@
+let sign = prompt("Оберіть бажану операцію (+, -, *, /)");
+let x = +prompt(`Введіть перше число`);
+let y = +prompt(`Введіть друге число`);
+
+switch (true) {
+    case !isNaN(x) && !isNaN(y):
+        switch (sign) {
+            case "+" :
+                console.log(`${x} ${sign} ${y} = ${x + y}`);
+                break;
+            case "-" :
+                console.log(`${x} ${sign} ${y} = ${x - y}`);
+                break;
+            case "*" :
+                console.log(`${x} ${sign} ${y} = ${x * y}`);
+                break;
+            case "/" :
+                if (y !== 0) {
+                    console.log(`${x} ${sign} ${y} = ${x / y}`);
+                } else {
+                    console.log('Ділення на нуль');
+                }
+                break;
+            default :
+                console.log(`Не вірне значення операції`);
+        }
+        break;
+    default :
+      console.log('Ви ввели не число')
+}
+
